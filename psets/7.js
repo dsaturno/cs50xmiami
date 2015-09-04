1abdda682a91594a086b1d039460207a1e05ca29
Daniel Saturno <daniel.saturno93@gmail.com>
2015-09-03 22:10:44 -0400

    not yet complete

--- psets/7.js
+++ psets/7.js
@@ -13,4 +13,17 @@ fixStart('babble'): 'ba**le'
 
 function fixStart(s) {
   // write your solution here...
+  var first_char = s.charAt(0);
+  var new_s = first_char;
+
+  for (var i = 1, n = s.length; i < n; i++) {
+    if (s.charAt(i) === first_char)
+      new_s = new_s.concat('*');
+    else
+      new_s = s.concat(charAt(i)); // this line returns an error
+  }
+    return new_s;
+
 }
+
+console.log(fixStart('babble'));
